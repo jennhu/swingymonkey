1190ea5910be0e5689f5c536b1a8b41bc5a8b1a5
elbertgong <elbertgong@college.harvard.edu>
2017-04-27 22:32:19 -0400

    cleaner run.py

--- code/run.py
+++ code/run.py
@@ -1,36 +1,34 @@
-# Imports.
+# for moving average
+from __future__ import division
+from pylab import plot, ylim, xlim, show, xlabel, ylabel, grid
+from numpy import linspace, loadtxt, ones, convolve
+
+# other imports
 import numpy as np
 import numpy.random as npr
-import gc # for garbage collection
 import matplotlib.pyplot as plt
-
 from SwingyMonkey import SwingyMonkey
-
 
 class Learner(object):
     def __init__(self):
         self.last_state  = None
         self.last_action = None
         self.last_reward = None
-        self.eps = 0.01
-        self.eta = 1
+        self.eps = 0
+        self.eta = 0.5
         self.xi = 1000
-        self.gamma = 0.9
+        self.gamma = 0.75
         self.grav_ind = None
-        self.n_dist_bins = 3
-        self.n_mtop_bins = 3
-        self.n_ttop_bins = 8
-        self.n_vel_bins = 3
+        self.n_dist_bins = 5
+        self.n_mtop_bins = 5
+        self.n_ttop_bins = 7
+        self.n_vel_bins = 5
         self.dist_bins = np.linspace(-120, 480, num=self.n_dist_bins)
         self.mtop_bins = np.linspace(0, 400, num=self.n_mtop_bins)
         self.ttop_bins = np.linspace(-150, 150, num=self.n_ttop_bins)
         self.vel_bins = np.linspace(-30, 20, num=self.n_vel_bins)
-        # self.w = np.zeros((4, self.nbins+1, self.nbins+1, self.nbins+1, self.nbins+1, 2))
         self.w = np.zeros((4, self.n_dist_bins+1, self.n_mtop_bins+1, self.n_ttop_bins+1, self.n_vel_bins+1, 2))
-        # self.w = np.concatenate((np.ones((4, self.nbins+1, self.nbins+1, self.nbins+1, self.nbins+1, 1)), np.zeros((4, self.nbins+1, self.nbins+1, self.nbins+1, self.nbins+1, 1))), axis=5)
         self.niters = 0
-        # self.D = 5
-        # self.w = [np.zeros((self.D, )) for i in range(self.D)]
 
     def reset(self):
         self.last_state  = None
@@ -44,23 +42,16 @@ class Learner(object):
         Return 0 if you don't want to jump and 1 if you do.
         '''
 
-        # Don't jump at beginning to determine gravity
+        # to determine gravity, don't jump at beginning
         if not self.last_state:
             self.last_action = 0
             self.last_state  = state
             return self.last_action
 
-        # Determine gravity
+        # determine gravity
         if self.last_action == 0:
             self.grav_ind = self.last_state['monkey']['vel'] - state['monkey']['vel'] - 1
-            # print(self.grav_ind)
 
-        # You might do some learning here based on the current state and the last state.
-
-        # You'll need to select and action and return it.
-        # Return 0 to swing and 1 to jump.
-
-        # new_action = npr.rand() < 0.1
         # epsilon greedy
         last_dist_ind = np.digitize(self.last_state['tree']['dist'], self.dist_bins)
         last_mtop_ind = np.digitize(self.last_state['monkey']['top'], self.mtop_bins)
@@ -72,27 +63,15 @@ class Learner(object):
         ttop_ind = np.digitize(state['tree']['top'] - state['monkey']['top'], self.ttop_bins)
         vel_ind = np.digitize(state['monkey']['vel'], self.vel_bins)
 
-        # last_state_ind_list = np.array([np.digitize(self.last_state['tree']['dist'], self.dist_bins), np.digitize(self.last_state['monkey']['top'], self.mtop_bins), \
-        #                 np.digitize(self.last_state['tree']['top'], self.ttop_bins), np.digitize(self.last_state['monkey']['vel'], self.vel_bins)])
-        # state_ind_list = np.array([np.digitize(state['tree']['dist'], self.dist_bins), np.digitize(state['monkey']['top'], self.mtop_bins), \
-        #                 np.digitize(state['tree']['top'], self.ttop_bins), np.digitize(state['monkey']['vel'], self.vel_bins)])
-        # last_state_ind = [[i] for i in last_state_ind_list]
-        # state_ind = [[i] for i in state_ind_list]
-
-        # print(self.Phi(self.last_state, self.last_action).shape)
-        
         # update
         new_action = 0
         self.niters = self.niters + 1
-        # print(self.eps * np.exp(self.niters / -100))
         if npr.rand() < self.eps * np.exp(self.niters / -10000):
             new_action = int(npr.rand() < 0.5)
         else:
             new_action = np.argmax(self.w[self.grav_ind, dist_ind, mtop_ind, ttop_ind, vel_ind, :])
-        # Q = np.dot(self.w, self.Phi(self.last_state, self.last_action))
         prev = self.w[self.grav_ind, last_dist_ind, last_mtop_ind, last_ttop_ind, last_vel_ind, self.last_action]
         grad = prev - (self.last_reward + self.gamma * max(self.w[self.grav_ind, dist_ind, mtop_ind, ttop_ind, vel_ind, :]))
-        # print(prev, grad)
         self.w[self.grav_ind, last_dist_ind, last_mtop_ind, last_ttop_ind, last_vel_ind, self.last_action] = prev - self.eta * np.exp(self.niters / -self.xi) * grad
 
         new_state  = state
@@ -134,29 +113,37 @@ def run_games(learner, hist, iters = 100, t_len = 100):
 
 if __name__ == '__main__':
 
-    gc.collect() #???
-    # Select agent.
-
-    agent = Learner(gamma)
-
-    assert(np.count_nonzero(agent.w) == 0)
+    agent = Learner()
 
     # Empty list to save history.
     hist = []
     # Run games.
     nepochs = 100
     run_games(agent, hist, nepochs, 0)
-    print(gamma, np.average(hist), np.std(hist))
+
+    # print(np.average(hist), np.std(hist))
+    # def movingaverage(interval, window_size):
+    # 	window= np.ones(int(window_size))/float(window_size)
+    # 	return np.convolve(interval, window, 'same')
+    # x = range(1,101)
+    # y = hist
+    # plot(x,y,"k.")
+    # y_av = movingaverage(y, 10)
+    # plot(x, y_av,"r")
+    # xlim(0,100)
+    # xlabel("Epoch")
+    # ylabel("Score")
+    # grid(True)
+    # show()
+    # plt.savefig('movingavg.png')
 
     # Save history.
-    # np.save('hist',np.array(hist))
+    np.save('hist',np.array(hist))
 
     # Plot histogram
     # plt.hist(hist)
     # plt.title("Performance with "+str(nepochs)+" epochs")
     # plt.xlabel("Score")
-    # output_file = '.png'
+    # output_file = 'histo.png'
     # plt.savefig(output_file)
     # plt.show()
-
-    
